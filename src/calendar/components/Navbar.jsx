8d6304ae31d65b4d0de214899311a824ba4efe47
= <=>
2023-11-14 15:20:55 -0600

    All Configuration Finished

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useAuthStore } from "../../hooks"
 
+import { Offline, Online } from 'react-detect-offline';
+
 export const Navbar = () => {
 
     const { startLogout, user } = useAuthStore();
@@ -11,6 +13,14 @@ export const Navbar = () => {
                 &nbsp;
                 {user.name}
             </span>
+
+            <Online>
+                <span className="text-success">Online</span>
+            </Online>
+            <Offline>
+                <span className="text-danger">Offline - Peticiones serán guardadas</span>
+            </Offline>
+
             <button className="btn btn-outline-danger" onClick={startLogout}>
                 <i className="fas fa-sign-out-alt"></i>
                 &nbsp;
